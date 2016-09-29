c37e1656973486c3a8fdbb49bc8c2a923290347b
Vinayak <vinayakkankanwadi@hotmail.com>
2016-09-30 04:08:47 +1000

    Updated to support guppy-pre-commitwith gulp-bump

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var bump = require('gulp-bump');
 
 gulp.task('pre-commit', function() {
-  console.log('hello world');
+  console.log('Bumpig up the versions');
+  gulp.src(['./package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
 });
